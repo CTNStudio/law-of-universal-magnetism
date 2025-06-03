c9adf0702a62a8fdc0e29711a4411e6a1d12b494
Wang-Xiao-Jing <1809315974@qq.com>
2025-06-03 18:30:53 +0800

    清理

---
+++ src/main/java/com/ctn/lawofuniversalmagnetism/LoUMDatagenMain.java
@@ -0,0 +1,29 @@
+package com.ctn.lawofuniversalmagnetism;
+
+import com.ctn.lawofuniversalmagnetism.datagen.LoUMTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.ctn.lawofuniversalmagnetism.LoUM.LOUM_ID;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, modid = LOUM_ID)
+public class LoUMDatagenMain {
+	@SubscribeEvent
+	public static void gatherData(GatherDataEvent event) {
+		DataGenerator generator = event.getGenerator();
+		PackOutput output = generator.getPackOutput();
+		ExistingFileHelper fileHelper = event.getExistingFileHelper();
+		CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+		LoUMTags.Block blockTags = new LoUMTags.Block(output, lookupProvider, fileHelper);
+		generator.addProvider(event.includeClient(), blockTags);
+		generator.addProvider(event.includeClient(), new LoUMTags.Item(output, lookupProvider, blockTags.contentsGetter(), fileHelper));
+
+	}
+}
